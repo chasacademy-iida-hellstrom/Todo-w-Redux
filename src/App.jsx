7550ae057abe07f42571724c9d4ddc50b7fc68c4
iida hellström <iidahellstrom@gmail.com>
2023-03-01 21:17:57 +0100

    second

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import "./App.css";
-import { todoList, add, remove, done } from "./todo-module";
+import { todoList, add, remove, done } from "./todos";
 
 function App() {
   //1. skapa todolist state
