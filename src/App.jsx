cca8b92f52473c873251ba76a726c4371b553ae6
iida hellström <iidahellstrom@gmail.com>
2023-03-01 21:15:05 +0100

    first

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import "./App.css";
+import { todoList, add, remove, done } from "./todo-module";
+
+function App() {
+  //1. skapa todolist state
+  //2. visa listan i html-koden. Använd list.map(). Se hacker stories.
+  //3. lägg till nya objekt till arrayen med ett input fält och en add knapp.
+  //
+
+  const [text, setText] = useState();
+
+  // spara till en variabel
+  function handleAdd(event) {
+    setText(event.target.value);
+  }
+
+  return (
+    <div className="App">
+      <input type="text" onChange={handleAdd} />
+      <button onClick={() => add({ title: text })}>Add</button>
+      {todoList().map((item) => {
+        return (
+          <div key={item.id}>
+            {item.title} {item.done}
+            <button onClick={() => done(item.id)}>
+              {item.done ? "Undo" : "Done"}
+            </button>
+            <button onClick={() => remove(item.id)}>Delete</button>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/todo-module.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { createReduxModule } from "hooks-for-redux";
+
+export const [todoList, { add, remove, done }] = createReduxModule("todo", [], {
+  add: (state, newTodo) => [
+    ...state,
+    { ...newTodo, id: state.length, title: newTodo.title, done: false },
+  ],
+  remove: (state, id) => state.filter((i) => i.id !== id),
+  done: (state, id) =>
+    state.map((todo) => {
+      if (todo.id === id) {
+        todo.done = !todo.done;
+      }
+      return todo;
+    }),
+});
